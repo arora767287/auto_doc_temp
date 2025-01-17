301116a1a9bb52b7858c20dc8954b69b0fa30543
Nitya Arora <arora767287@sas.edu.sg>
2025-01-16 17:05:51 -0800

    logo added

--- landing_page/src/components/Navbar.jsx
+++ landing_page/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import logo from "./logo.png";
 
 const Navbar = () => {
     return (
         <nav className="bg-white shadow py-4">
             <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
-                <h1 className="text-2xl font-bold text-blue-600">Peppr AI</h1>
+                <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                    <img src={logo} style={{width: 60}}/>
+                    <h1 className="text-2xl font-bold text-pink-600">Peppr AI</h1>
+                </div>
                 <div className="space-x-6">
-                    <a href="#problem" className="text-gray-600 hover:text-blue-600">Problem</a>
-                    <a href="#solution" className="text-gray-600 hover:text-blue-600">Solution</a>
+                    <a href="#problem" className="text-gray-600 hover:text-pink-600">Problem</a>
+                    <a href="#solution" className="text-gray-600 hover:text-pink-600">Solution</a>
                     {/* <a href="#pricing" className="text-gray-600 hover:text-blue-600">Pricing</a> */}
-                    <a href="https://calendly.com/nitya-meeting-times/10-minute-meeting-with-nitya" className="bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-blue-700">
+                    <a href="https://calendly.com/nitya-meeting-times/10-minute-meeting-with-nitya" className="bg-pink-600 text-white px-4 py-2 rounded-full hover:bg-blue-700">
                         Book a Demo
                     </a>
                 </div>
